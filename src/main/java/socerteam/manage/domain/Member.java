56a5983b1cc53ce5f83a8c584076711acb2e7e57
Eco-MIn <guldor10.b@gmail.com>
2021-05-12 15:34:43 +0900

    domain

--- src/main/java/socerteam/manage/domain/Member.java
+++ src/main/java/socerteam/manage/domain/Member.java
@@ -2,13 +2,14 @@ package socerteam.manage.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
-@Getter
+@Getter()
 public class Member extends BaseEntity{
 
     @Id
--- src/test/java/socerteam/manage/repository/MemberRepositoryTest.java
+++ src/test/java/socerteam/manage/repository/MemberRepositoryTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Rollback
+@Rollback(value = false)
 class MemberRepositoryTest {
 
     @Autowired
